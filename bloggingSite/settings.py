961d9b1ae83bb3b14d28a8d2c8778177e3b41fc5
Debanjan Paul <70275844+paulDebanjan@users.noreply.github.com>
2024-02-12 11:31:05 +0600

    v1.2
    document ready to deploy.

--- bloggingSite/settings.py
+++ bloggingSite/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-il8u36z5h$q8bnst+f5$mn*ee9vzupr*pz@z7m#xw*c=2@@k7h
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
